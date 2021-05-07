362559f8b21b952af2b20d175f573ac523726973
Teddy <teddysaputra@hotmail.com>
2021-05-07 21:57:36 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './1.jpg';
 import './App.css';
 
 function App() {
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <div className="Centered">
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
