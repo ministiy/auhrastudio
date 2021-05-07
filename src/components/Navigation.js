605445a2d482d387fd10c242458404562c46c1a3
Teddy <teddysaputra@hotmail.com>
2021-05-07 23:03:09 +0800

    Change

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -46,7 +46,7 @@ function Navigation(props) {
                 }`}
               >
                 <Link class="nav-link" to="/portfolio">
-                  Portfolio
+                  Portfolioss
                 </Link>
               </li>
             </ul>
